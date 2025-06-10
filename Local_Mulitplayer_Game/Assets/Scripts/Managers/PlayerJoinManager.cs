946915ec3285ef70640734480e9c5b13d3619f33
DG-studios29 <70883550+DG-studios29@users.noreply.github.com>
2025-06-10 16:56:04 +0200

    adding some ui

--- Local_Mulitplayer_Game/Assets/Scripts/Managers/PlayerJoinManager.cs
+++ Local_Mulitplayer_Game/Assets/Scripts/Managers/PlayerJoinManager.cs
@@ -3,21 +3,27 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Users;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.InputSystem.Utilities;
+using TMPro;
+using static UnityEngine.Rendering.DebugUI;
+using System.Collections;
 
 public class PlayerJoinManager : MonoBehaviour
 {
     public PlayerInputManager playerInputManager;
     public GameObject joinPanel; 
     public GameObject heroPanel; 
-    public GameObject continueButton; 
+    public GameObject continueButton;
+    public GameObject noEnoughPlayers;
     private PlayerControls inputActions;
     private InputAction joinAction;
     private int joinedPlayers = 0;
+    public float showDuration = 4f;
 
     private void Awake()
     {
         inputActions = new PlayerControls();
         joinAction = inputActions.Player.PlayerJoin;
+        noEnoughPlayers.SetActive(false);
     }
 
     private void OnEnable()
@@ -54,7 +60,9 @@ public class PlayerJoinManager : MonoBehaviour
     {
         if (joinedPlayers < 2)
         {
+            noEnoughPlayers.SetActive(true);
             Debug.LogWarning("[JoinManager] Not enough players to continue.");
+            StartCoroutine(HideAfterDelay());
             return;
         }
 
@@ -62,5 +70,11 @@ public class PlayerJoinManager : MonoBehaviour
 
         joinPanel.SetActive(false);
         heroPanel.SetActive(true);
+    }
+
+    private IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(showDuration);
+        noEnoughPlayers.SetActive(false);
     }
 }
